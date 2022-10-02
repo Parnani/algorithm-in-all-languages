e4a1982b5ff27bda592581b297515300fbc72449
Alexis Chân Gridel <alexis.gridel@pm.me>
2022-10-02 09:29:13 +0200

    feat(java): add bubble sort algorithm

---
+++ .java/BubbleSortAlgorithm.java
@@ -0,0 +1,19 @@
+public class BubbleSortAlgorithm {
+    public static void main(String[] args) {
+        int[] arr = { 5, 1, 4, 2, 8 };
+        int n = arr.length;
+        int temp = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < (n - i); j++) {
+                if (arr[j - 1] > arr[j]) {
+                    temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
